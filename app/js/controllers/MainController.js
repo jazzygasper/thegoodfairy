cd499e43060edbd778f359ea0784ed5d0f97022a
Jazzy <jasmina.gasper@gmail.com>
2016-09-15 15:57:10 +0100

    first karma test passing

---
+++ app/js/controllers/MainController.js
@@ -0,0 +1,10 @@
+thegoodfairyApp.controller('MainController', function() {
+  var self = this;
+
+  self.services = [];
+
+  self.findServices = function(){
+    self.services;
+  }
+
+});
--- test/unit/MainController.spec.js
+++ test/unit/MainController.spec.js
@@ -1,5 +1,19 @@
 describe('MainController', function() {
+  beforeEach(module('thegoodfairyApp'));
 
+  var ctrl;
+
+  beforeEach(inject(function($controller) {
+  ctrl = $controller('MainController');
+
+}));
+
+
+  it('starts with empty array', function(){
+    // scope.$apply();
+    ctrl.findServices();
+    expect(ctrl.services).toEqual([]);
+  });
 
 
 });
