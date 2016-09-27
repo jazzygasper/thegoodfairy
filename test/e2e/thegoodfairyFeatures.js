18e87a189ffd6497d0ec838844be1d6ff5e2e569
Jazzy <jasmina.gasper@gmail.com>
2016-09-27 17:08:33 +0100

    feature test passing

--- test/e2e/thegoodfairyFeatures.js
+++ test/e2e/thegoodfairyFeatures.js
@@ -4,9 +4,12 @@ describe('thegoodfairy', function() {
     expect(browser.getTitle()).toEqual('The Good Fairy');
   });
 
-  it('has input box', function() {
+  it('shows geographic coordinates', function() {
     browser.get('/');
     $('#input').sendKeys('E125ET');
+    $('#submit').click();
+    var geographicCoordinates = element.all(by.repeater('geographicCoordinates in controller.geographicCoordinates'));
+    expect(geographicCoordinates.first().element(by.css('.lat')).getText()).toEqual('51.5613656682057');
   })
 
 });
